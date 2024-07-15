698d49aa17c49a829eaf4147d2cfb4f993dc3aba
AlexRiosXI <alexriosxi@gmail.com>
2024-07-15 08:55:46 -0600

    [FIX] user path hardcode remove

--- setup.sh
+++ setup.sh
@@ -2,21 +2,21 @@
 user=$(whoami)
 echo "Starting setup process for user $user"
 user_directory="/home/$user"
-config_directory="$user_directory/perro"
+config_directory="$user_directory/.config"
 #Descargar awesome
-#sudo apt update
-#sudo apt install awesome
-#sudo apt install awesome-extra
-#sudo apt install awesome-doc
-#sudo apt install nitrogen
-#sudo apt install polybar
-#sudo apt install zsh
-#sudo apt install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev meson ninja-build uthash-dev
-
-#git clone https://github.com/yshui/picom 
-#cd picom
-#meson setup --buildtype=release build
-#sudo ninja -C build install
+sudo apt update
+sudo apt install awesome
+sudo apt install awesome-extra
+sudo apt install awesome-doc
+sudo apt install nitrogen
+sudo apt install polybar
+sudo apt install zsh
+sudo apt install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev meson ninja-build uthash-dev
+sudo apt install cmake
+git clone https://github.com/yshui/picom 
+cd picom
+meson setup --buildtype=release build
+sudo ninja -C build install
 if [ -d "$config_directory" ]; then
 
   echo "config folder exists, copying config files into it."
